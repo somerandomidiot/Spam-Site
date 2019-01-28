ac9c6d3023c99977579bb3fd5e24036aa69cff6f
somerandomidiot <47123973+somerandomidiot@users.noreply.github.com>
2019-01-28 21:50:29 +0000

    Add files via upload

--- JS/signup.js
+++ JS/signup.js
@@ -1,10 +1,8 @@
-let role;
-
 let usernames = [];
 let passwords = [];
-let adminnames = ["alex"]; // You're looking at the code, wow.
+let adminnames = ["Alex"]; // You're looking at the code, wow.
 let adminpasses = ["yoyoyoyo"];
-let signupname, signuppass;
+let signupname, signuppass, role;
 
 function processsignup() {
 // Get signup information
@@ -14,11 +12,13 @@ signuppass = document.getElementById('signuppass').value.trim();
 // Checks
 if (signupname.length < 3 || signupname.length > 20) return error(`Please enter a username that is 3-20 characters long. (${signupname.length} characters detected)`);
 if (signuppass.length < 6 || signuppass.length > 25) return error(`Please enter a password that is 6-25 characters long. (${signuppass.length} characters detected)`);
-if (signupname == adminnames && signuppass != adminpasses) return error(`You can not login as an Administrator without a valid password.`)
+if (signupname == adminnames && signuppass != adminpasses) return error(`You can not signup using an Administrator username unless you enter the account's password.`)
+
 // Passed the checks, let's store the temporary information in an array
 error(``);
 usernames.push(signupname)
 passwords.push(signuppass)
+alert(`Temporary account created successfully.`)
 
 document.getElementById('signupdiv').style = "display: none;"
 document.getElementById('logindiv').style = "display: block;"
