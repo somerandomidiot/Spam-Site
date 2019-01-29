8ec16fd1c1670b8c4c66f91626609f9bb1886e5e
somerandomidiot <47123973+somerandomidiot@users.noreply.github.com>
2019-01-29 12:58:01 +0000

    Update killbrowser.js

--- JS/killbrowser.js
+++ JS/killbrowser.js
@@ -1,7 +1,11 @@
 function killbrowser() {
 let x = confirm(`You are about to kill your browser by infinitely spamming logs in the console. Are you sure you want to do this?`);
 if (x == true) {
-for (let i = 0; i < Infinity; i++)
-console.log(`Killed`), i;
+setInterval(function() {
+spam() 
+}, 1;
 }
+}         
+function spam() {
+console.log(`Killed`);
 }
